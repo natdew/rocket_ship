181cd4d34f3fb408ff570c4af5289d220b19c14a
natdew <natashadewitz@gmail.com>
2015-10-22 14:27:40 -0400

    --

--- rocket_ship_start/sketch.js
+++ rocket_ship_start/sketch.js
@@ -1,10 +1,42 @@
+var backgroundImage;
+var rocketImage;
+var platformSmokeImage;
+var smokeImage;
+
+function preload() {
+	backgroundImage = loadImage("images/Background.jpg");
+	rocketImage = loadImage("images/rocket.png");
+	platformSmokeImage = loadImage("images/platform_smoke.png")
+	smokeImage = loadImage("images/smoke.png")
+}
+
 function setup() {
 	createCanvas(800, 600);
 }
 
 function draw() {
-	background(150, 200, 150);
-	fill(200, 255, 200);
-	noStroke();
-	ellipse(400, 300, 300, 300);
+
+	image(backgroundImage, 0, 0);
+	var rocketY = 320
+	if (mouseIsPressed) {
+		rocketY = mouseY;
+		if (rocketY > 320){
+			rocketY = 320;
+		}
+	}
+
+if (mouseIsPressed) {
+	image(smokeImage, 420, 180)
+	image(platformSmokeImage, 390, 420);
+
+}
+fill(255, 0, 0);
+rectMode(CORNERS);
+noStroke();
+fill(218, 227, 211);
+if (rocket Y > 100) {
+rect(300, 180, 600, rocketY + 100)
+}
+image(rocketImage, 420, rocketY)
+
 }
